73e0b6950eabfa0886ecdb51083156e784e1f4a3
Victor Egorov <qrioflat@gmail.com>
2018-05-17 00:04:03 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j;
 
+/**
+*Calculate.
+*
+*@author Victor Egorov (qrioflat@gmail.com)
+*/
 public class Calculate {
 
+	/**
+	*Main method, write "Hello World!" in console.
+	*@param args - args
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
