37ff9614e403e1fa0d296b020974d5549a5a359a
Victor Egorov <qrioflat@gmail.com>
2018-05-30 15:50:42 +0300

    Контрольное задание. Объединение отсортированных массивов. Упрощено.

--- chapter_001/src/main/java/ru/job4j/array/ArraySort.java
+++ chapter_001/src/main/java/ru/job4j/array/ArraySort.java
@@ -19,32 +19,22 @@ public class ArraySort {
      */
     public int[] sort(int[] first, int[] second) {
         result = new int[first.length + second.length];
-        check(first, second, 0, 0, 0);
-        return this.result;
-    }
-
-    /**
-     * Main sort algorithm.
-     * @param first first array.
-     * @param second second array.
-     * @param firstPos position to check of first array.
-     * @param secondPos position to check of second array.
-     * @param index position in new array.
-     */
-    private void check(int[] first, int[] second, int firstPos, int secondPos, int index) {
-        if (firstPos < first.length && secondPos < second.length) {
+        int firstPos = 0;
+        int secondPos = 0;
+        int index = 0;
+        while (firstPos < first.length && secondPos < second.length) {
             if (first[firstPos] < second[secondPos]) {
-                this.result[index] = first[firstPos];
-                check(first, second, firstPos + 1, secondPos, ++index);
+                this.result[index++] = first[firstPos++];
             } else {
-                this.result[index] = second[secondPos];
-                check(first, second, firstPos, secondPos + 1, ++index);
+                this.result[index++] = second[secondPos++];
             }
-        } else if (secondPos < second.length) {
+        }
+        if (secondPos < second.length) {
             close(index, second, secondPos);
         } else if (firstPos < first.length) {
             close(index, first, firstPos);
         }
+        return this.result;
     }
 
     /**
