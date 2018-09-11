fd51cb01f750c44f9186fbba190ed8c0b456665a
Victor Egorov <qrioflat@gmail.com>
2018-09-11 16:51:47 +0300

    1. Подсчет функции в диапазоне. [#24251]

--- chapter_002/src/main/java/ru/job4j/lambda/Calculator.java
+++ chapter_002/src/main/java/ru/job4j/lambda/Calculator.java
@@ -1,7 +1,10 @@
 package ru.job4j.lambda;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Calculator {
 
@@ -11,5 +14,14 @@ public class Calculator {
         for (int index = start; index != finish; index++) {
             media.accept(op.apply(value, index));
         }
+    }
+
+    public List<Double> diapason(int start, int end,
+                                 Function<Double, Double> func) {
+        List<Double> result = new ArrayList<>(100);
+        for (double index = start; index < end; index++) {
+            result.add(func.apply(index));
+        }
+        return result;
     }
 }
--- chapter_002/src/test/java/ru/job4j/lambda/CalculatorTest.java
+++ chapter_002/src/test/java/ru/job4j/lambda/CalculatorTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -30,5 +32,37 @@ public class CalculatorTest {
                 result -> buffer.add(result)
         );
         assertThat(buffer, is(Arrays.asList(2D, 4D, 6D)));
+    }
+
+    @Test
+    public void whenLineThen468() {
+        Calculator calc = new Calculator();
+        List<Double> buffer = calc.diapason(
+                1, 4,
+                x -> x * 2 + 2
+        );
+        assertThat(buffer, is(Arrays.asList(4D, 6D, 8D)));
+    }
+
+    @Test
+    public void whenSquareThen468() {
+        Calculator calc = new Calculator();
+        List<Double> buffer = calc.diapason(
+                1, 4,
+                x -> x * x - x - 2
+        );
+        assertThat(buffer, is(Arrays.asList(-2D, 0D, 4D)));
+    }
+
+    @Test
+    public void whenLogThen468() {
+        Calculator calc = new Calculator();
+        List<Double> buffer = calc.diapason(
+                0, 2,
+                x -> 1 / Math.exp(x)
+        );
+        assertThat(buffer.size(), is(2));
+        assertThat(buffer.get(0), is(1D));
+        assertThat(buffer.get(1), closeTo(0.367, 0.001));
     }
 }
