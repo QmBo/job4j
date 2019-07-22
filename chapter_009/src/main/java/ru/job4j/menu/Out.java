b3181a4e06e42c3b0aa24c85870752f5c0835618
Victor Egorov <qrioflat@gmail.com>
2019-07-22 15:27:54 +0300

    Создать меню. [ #4748]

---
+++ chapter_009/src/main/java/ru/job4j/menu/Out.java
@@ -0,0 +1,14 @@
+package ru.job4j.menu;
+/**
+ * Out
+ * @author Victor Egorov (qrioflat@gmail.com).
+ * @version 0.1
+ * @since 22.07.2019
+ */
+public interface Out {
+    /**
+     * Name printer.
+     * @return position and name.
+     */
+    String printName();
+}
